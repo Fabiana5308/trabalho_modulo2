a9a47f00f343822797e15a307d536da180897d67
Fabiana Alves <fabileite653@gmail.com>
2023-05-19 14:43:47 -0300

    Evidência de entrega
    subindo evidência de entrega do trabalho.

---
+++ Evidência_de_entrega.py
@@ -0,0 +1,13 @@
+# Olá! Aqui está a minha solução para o problema proposto:
+
+# 1. Definir uma lista de candidatos com seus respectivos resultados em cada etapa do processo seletivo (entrevista, teste teórico, teste prático e avaliação de soft skills).
+
+# 2. Criei a função "buscar candidatos" que recebe como parâmetros as notas de cada etapa do processo seletivo e faz uma busca pelos candidatos que atendem a esses requisitos.
+
+# 3. Criei a função "buscar candidatos por notas" que solicita as notas do usuário e lista os candidatos que possuem notas iguais ou superiores a essas.
+
+# Acredito que essa solução atende às especificações solicitadas. Qualquer dúvida ou ajuste, estou à disposição!
+
+# Atenciosamente,
+
+# Fabiana Alves.
